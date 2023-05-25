c5e824d642a73ff1c5205e2a336759a05bf6722b
Fernando Ramos <fndo.ramos@gmail.com>
2023-05-25 16:58:58 +0200

    changed the idea of my game. added more enemies shooting at player, smooth movement and meteors falling

--- js/main.js
+++ js/main.js
@@ -3,11 +3,17 @@ class Game {
     this.newPlayer = null;
     this.newGround = null;
     this.newEnemy = null;
-    this.exitDoor = null;
-    this.newBullet = null;
-    this.fallTimer = null;
-    this.isFfalling = true;
+    this.newBullet1 = null;
+    this.newBullet2 = null;
+    this.newBullet3 = null;
     this.bulletsArr = [];
+    this.bulletsArr2 = [];
+    this.bulletsArr3 = [];
+    this.meteorArr = [];
+    this.timeIdBullets1 = null;
+    this.timeIdBullets2 = null;
+    this.timeIdBullets3 = null;
+    this.enemiesArr = [];
 
     
 
@@ -17,131 +23,105 @@ class Game {
   startGame() {
     this.newPlayer = new Player();
     this.newGround = new Ground();
-    this.newEnemy = new Enemy();
-    this.exitDoor = new ExitDoor();
+    
 
     this.eventListenerMove();
-    //this.fall();
 
-    setTimeout(() => {
+    for (let i = 0; i < 3; i++) {
+      this.newEnemy = new Enemy(i);
+      this.enemiesArr.push(this.newEnemy);
+      this.enemiesArr[i].positionX += this.newEnemy.positionX + 10;
+      if (this.enemiesArr.length > 1)
+      this.enemiesArr[i].positionX += this.enemiesArr[i - 1].positionX + 20;
+      console.log(this.enemiesArr);
+    }
+
+   
+
+                  //bullets for enemy1
+    this.timeIdBullets1 = setTimeout(() => {
       setInterval(() => {
-        this.newBullet = new Bullets();
-        this.bulletsArr.push(this.newBullet);
-        //console.log(this.bulletsArr);
+        this.newBullet1 = new Bullets1();
+        this.bulletsArr.push(this.newBullet1);
       }, 2000);
-    }, 2500);
+    }, 3500);
 
     setInterval(() => {
-      this.bulletsArr.forEach((bulletElm) => {
-        bulletElm.moveDown(this.newPlayer.positionX, this.newPlayer.positionY);
+      this.bulletsArr.forEach((bulletElm, index) => {
+        bulletElm.moveDown(this.newPlayer.positionX);
         this.detectBulletColisionWithPlayer(bulletElm);
-        this.deleteBullets(bulletElm);
+        this.deleteBullets(bulletElm, index);
       });
     }, 60);
+                  //bullets for enemy2
+    this.timeIdBullets2 = setTimeout(() => {
+      setInterval(() => {
+        this.newBullet2 = new Bullets2();
+        this.bulletsArr.push(this.newBullet2);
+      }, 2000);
+    }, 4500);
+    /*
+    setInterval(() => {
+      this.bulletsArr.forEach((bulletElm, index) => {
+        bulletElm.moveDown(this.newPlayer.positionX);
+        this.detectBulletColisionWithPlayer(bulletElm);
+        this.deleteBullets(bulletElm, index);
+      });
+    }, 60);
+    */
+                  //bullets for enemy3
+    this.timeIdBullets3 = setTimeout(() => {
+      setInterval(() => {
+        this.newBullet3 = new Bullets3();
+        this.bulletsArr.push(this.newBullet3);
+      }, 2000);
+    }, 5500);
+    /*
+    setInterval(() => {
+      this.bulletsArr.forEach((bulletElm, index) => {
+        bulletElm.moveDown(this.newPlayer.positionX);
+        this.detectBulletColisionWithPlayer(bulletElm);
+        this.deleteBullets(bulletElm, index);
+      });
+    }, 60);
+    */
+    
+                //meteors start falling
+    setTimeout(() => {
+      setInterval(() => {
+        this.newMeteor = new Meteor();
+        this.meteorArr.push(this.newMeteor);
+        //console.log(this.meteorArr)
+      }, 3000);
+    }, 10000);
+
+    setInterval(() => {
+      this.meteorArr.forEach((meteorElm) => {
+        //console.log(this.meteorArr);
+        meteorElm.moveDown();
+        this.detectBulletColisionWithPlayer(meteorElm);
+        this.deleteMeteors(meteorElm);
+      })
+    }, 60);
+
+    
   }
 
   eventListenerMove() {
     document.addEventListener("keydown", (e) => {
-      //this.detectColisionExitDoor();
-      //this.fall();
       if (e.code === "ArrowRight") {
-        this.moveRigth();
+        this.newPlayer.moveRigth();
         console.log(this.newPlayer);
         
       } else if (e.code === "ArrowLeft") {
-        this.moveLeft();
+        this.newPlayer.moveLeft();
         console.log(this.newPlayer);
         
       } else if (e.code === "ArrowUp") {
-        this.jump();
+        this.newPlayer.jump();
         console.log(this.newPlayer);
       }
     });
-  }
-
-  moveRigth() {
-    if (this.newPlayer.isGoingLeft) {
-      clearInterval(this.newPlayer.leftTimerId)
-      this.newPlayer.isGoingLeft = false;
-    }
-    this.newPlayer.isGoingRigth = true;
-    this.newPlayer.rightTimerId = setInterval(() => {
-      this.newPlayer.positionX += 1;
-      this.newPlayer.playerDom.style.left = this.newPlayer.positionX + "vw";
-      this.detectColisionPlayerWithGroundBox();
-      if (this.newPlayer.positionX >= 100 - this.newPlayer.width) {
-        clearInterval(this.newPlayer.rightTimerId);
-      }
-    }, 80)
-      
-
-  }
-
-  moveLeft() {
-    if (this.newPlayer.isGoingRigth) {
-      clearInterval(this.newPlayer.rightTimerId);
-      this.newPlayer.isGoingRigth = false;
-    }
-    this.newPlayer.isGoingLeft = true;
-    this.newPlayer.leftTimerId = setInterval(() => {
-      this.newPlayer.positionX -= 1;
-      this.newPlayer.playerDom.style.left = this.newPlayer.positionX + "vw";
-      this.detectColisionPlayerWithGroundBox();
-      if (this.newPlayer.positionX <= 0) {
-        clearInterval(this.newPlayer.leftTimerId);
-      }
-    }, 80)
-  }
-
-  jump() {
-    if (this.newPlayer.isJumping) {
-      return;
-    }
-    this.newPlayer.jumpUpTimerId = setInterval(() => {
-      if (this.newPlayer.positionY > 30) {
-        clearInterval(this.newPlayer.jumpUpTimerId);
-        this.newPlayer.jumpDownTimerId = setInterval(() => {
-          if (this.newPlayer.positionY === 30) {
-            clearInterval(this.newPlayer.jumpDownTimerId);
-            this.newPlayer.isJumping = false;
-          }
-          this.newPlayer.positionY -= 15;
-          this.newPlayer.playerDom.style.bottom = this.newPlayer.positionY + "vh";
-        }, 40)
-      }
-      this.newPlayer.isJumping = true;
-      this.newPlayer.positionY += 15;
-      this.newPlayer.playerDom.style.bottom = this.newPlayer.positionY + "vh";
-    }, 40)
-  }
-
-  detectColisionExitDoor() {
-    if (
-      this.exitDoor.positionX < this.newPlayer.positionX + this.newPlayer.width &&
-      this.exitDoor.positionX + this.exitDoor.width > this.newPlayer.positionX &&
-      this.exitDoor.positionY < this.newPlayer.positionY + this.newPlayer.height &&
-      this.exitDoor.height + this.exitDoor.positionY > this.newPlayer.positionY
-    ) {
-      console.log("colision with door detected");
-      location.href = "./lvl2.html";
-    }
-  }
-
-  fall() {
-    if (this.isFfalling) {
-      this.fallTimer = setInterval(() => {
-        this.newPlayer.positionY -= 1;
-        this.newPlayer.playerDom.style.bottom = this.newPlayer.positionY + "vh";
-        this.detectColisionPlayerWithGroundBox();
-
-        if (this.newPlayer.playerDom.style.bottom === 0 - this.newPlayer.height + "vh") {
-          clearInterval(this.fallTimer);
-          this.newPlayer.playerDom.remove();
-          this.isFfalling = false;
-          location.href = "./lvl3.html";
-        }
-      }, 20);
-    }
   }
 
   detectBulletColisionWithPlayer(bulletElm) {
@@ -151,69 +131,46 @@ class Game {
       bulletElm.positionY < this.newPlayer.positionY + this.newPlayer.height &&
       bulletElm.height + bulletElm.positionY > this.newPlayer.positionY
     ) {
-      //console.log("colision with player detected");
+      console.log("colision with player detected");
       //location.href = "./lvl3.html";
     }
   }
 
-  detectColisionPlayerWithGroundBox() {
-    //this.groundContainer = document.querySelector("#groundContainer")
-    this.groundBoxArr = document.querySelectorAll(".groundBox");
-    //console.log(this.groundContainer);
-    //console.log(this.groundBoxArr);
-
-    this.groundBoxArr.forEach((boxElm) => {
-      this.boxHeightInVh = Math.round((boxElm.offsetHeight / window.innerHeight) * 100);
-      this.boxWidthInVw = Math.round((boxElm.offsetWidth / window.innerWidth) * 100);
-      this.boxPositionXInVw = Math.round((boxElm.offsetLeft / window.innerWidth) * 100);
-      this.boxPositionYInVh = Math.round((boxElm.offsetTop / window.innerHeight) * 100);
-      //console.log(this.boxHeightInVh);
-      //console.log(this.boxPositionYInVh);
-      //console.log(this.boxWidthInVw);
-      //console.log(this.boxPositionXInVw);
-
-      if (
-        (this.newPlayer.positionX + this.newPlayer.width / 2) < this.boxPositionXInVw + this.boxWidthInVw &&
-        this.newPlayer.positionX + this.newPlayer.width / 2 > this.boxPositionXInVw &&
-        this.newPlayer.positionY <= this.boxPositionYInVh + this.boxHeightInVh &&
-        this.newPlayer.height + this.newPlayer.positionY >= this.boxPositionYInVh
-      ) {
-        console.log("colision with ground detected");
-        clearInterval(this.fallTimer);
-        this.isFfalling = false;
-        return;
-      } else {
-        this.isFfalling = true;
-        //this.fall(); 
-      }
-    });
-  }
-
-  deleteBullets(bulletElm) {
+  deleteBullets(bulletElm, index) {
     if (
-      bulletElm.positionY < 15 - bulletElm.height ||
+      bulletElm.positionY < 8 - bulletElm.height ||
       bulletElm.positionX < 0 - bulletElm.width ||
       bulletElm.positionX > 100 + bulletElm.width
     ) {
-      this.bulletsArr.shift();
+      this.bulletsArr.splice(index, 1);
       bulletElm.bulletDom.remove();
     }
   }
+
+  deleteMeteors(meteorElm) {
+    if (
+      meteorElm.positionY < 8 - meteorElm.height ||
+      meteorElm.positionX < 0 - meteorElm.width ||
+      meteorElm.positionX > 100 + meteorElm.width
+    ) {
+      this.meteorArr.splice(0, 1);
+      meteorElm.meteorDom.remove();
+    }
+  }
+
 }
 
 class Player {
   constructor() {
-    this.positionX = 1;
-    this.positionY = 15;
     this.width = 2;
     this.height = 4;
+    this.positionX = 50 - this.width / 2;
+    this.positionY = 8;
     this.playerDom = null;
     this.rightTimerId = null;
     this.leftTimerId = null;
     this.jumpUpTimerId = null;
     this.jumpDownTimerId = null;
-    this.isGoingUp = false;
-    this.isGoingDown = false;
     this.isGoingLeft = false;
     this.isGoingRigth = false;
     this.isJumping = false;
@@ -232,23 +189,85 @@ class Player {
     this.playerParent = document.getElementById("board");
     this.playerParent.appendChild(this.playerDom);
   }
+
+  moveRigth() {
+    if (this.isGoingRigth) {
+      return;
+    }
+    if (this.isGoingLeft) {
+      clearInterval(this.leftTimerId)
+      this.isGoingLeft = false;
+    }
+    this.isGoingRigth = true;
+    this.rightTimerId = setInterval(() => {
+      this.positionX += 1;
+      this.playerDom.style.left = this.positionX + "vw";
+      if (this.positionX >= 100 - this.width) {
+        clearInterval(this.rightTimerId);
+      }
+    }, 80)
+      
+
+  }
+
+  moveLeft() {
+    if (this.isGoingLeft) {
+      return;
+    }
+    if (this.isGoingRigth) {
+      clearInterval(this.rightTimerId);
+      this.isGoingRigth = false;
+    }
+    this.isGoingLeft = true;
+    this.leftTimerId = setInterval(() => {
+      this.positionX -= 1;
+      this.playerDom.style.left = this.positionX + "vw";
+      if (this.positionX <= 0) {
+        clearInterval(this.leftTimerId);
+      }
+    }, 80)
+  }
+
+  jump() {
+    if (this.isJumping) {
+      return;
+    }
+    this.jumpUpTimerId = setInterval(() => {
+      if (this.positionY > 30) {
+        clearInterval(this.jumpUpTimerId);
+        this.jumpDownTimerId = setInterval(() => {
+          if (this.positionY < 10) {
+            clearInterval(this.jumpDownTimerId);
+            this.isJumping = false;
+          }
+          this.positionY -= 1;
+          this.playerDom.style.bottom = this.positionY + "vh";
+        }, 20)
+      }
+      this.isJumping = true;
+      this.positionY += 1;
+      this.playerDom.style.bottom = this.positionY + "vh";
+    }, 20)
+  }
 }
 
 class Enemy {
-  constructor() {
+  constructor(index) {
     this.width = 6;
     this.height = 4;
-    this.positionX = 50 - this.width / 2;
-    this.positionY = 100 - this.height;
+    this.positionX = 0;
+    this.positionY = 100 - this.height / 2;
     this.enemyDom = null;
+    this.index = index
 
+     
     this.createEnemy();
   }
 
   createEnemy() {
     this.enemyDom = document.createElement("div");
     const enemyTimeOutID = setTimeout(() => {
-      this.enemyDom.id = "enemy";
+      this.enemyDom.id = "enemy" + this.index;
       this.enemyDom.style.height = this.height + "vh";
       this.enemyDom.style.width = this.width + "vw";
       this.enemyDom.style.left = this.positionX + "vw";
@@ -269,11 +288,11 @@ class Enemy {
   }
 }
 
-class Bullets {
+class Bullets1 {
   constructor() {
     this.width = 1;
     this.height = 2;
-    this.positionX = 50 - this.width / 2;
+    this.positionX = 13 - this.width / 2;
     this.positionY = 78;
     this.bulletDom = null;
 
@@ -287,77 +306,155 @@ class Bullets {
     this.bulletDom.style.width = this.width + "vw";
     this.bulletDom.style.left = this.positionX + "vw";
     this.bulletDom.style.bottom = this.positionY + "vh";
-    this.bulletParent = document.getElementById("enemy");
+    this.bulletParent = document.getElementById("enemy0");
     this.bulletParent.appendChild(this.bulletDom);
   }
 
-  moveDown(x, y) {
+  moveDown(x) {
     if (this.positionX > x) {
-      this.positionY -= 0.7;
+      this.positionY -= 1;
       this.positionX -= 0.5;
       this.bulletDom.style.left = this.positionX + "vw";
       this.bulletDom.style.bottom = this.positionY + "vh";
     } else if (this.positionX < x) {
-      this.positionY -= 0.7;
+      this.positionY -= 1;
       this.positionX += 0.5;
       this.bulletDom.style.left = this.positionX + "vw";
       this.bulletDom.style.bottom = this.positionY + "vh";
     } else {
-      this.positionY -= 0.7;
+      this.positionY -= 1;
       this.bulletDom.style.bottom = this.positionY + "vh";
     }
+  }
+}
+
+class Bullets2 {
+  constructor() {
+    this.width = 1;
+    this.height = 2;
+    this.positionX = 43 - this.width / 2;
+    this.positionY = 78;
+    this.bulletDom = null;
+
+    this.createBullets();
+  }
+
+  createBullets() {
+    this.bulletDom = document.createElement("div");
+    this.bulletDom.className = "bullet";
+    this.bulletDom.style.height = this.height + "vh";
+    this.bulletDom.style.width = this.width + "vw";
+    this.bulletDom.style.left = this.positionX + "vw";
+    this.bulletDom.style.bottom = this.positionY + "vh";
+    this.bulletParent = document.getElementById("enemy1");
+    this.bulletParent.appendChild(this.bulletDom);
+  }
+
+  moveDown(x) {
+    if (this.positionX > x) {
+      this.positionY -= 1;
+      this.positionX -= 0.5;
+      this.bulletDom.style.left = this.positionX + "vw";
+      this.bulletDom.style.bottom = this.positionY + "vh";
+    } else if (this.positionX < x) {
+      this.positionY -= 1;
+      this.positionX += 0.5;
+      this.bulletDom.style.left = this.positionX + "vw";
+      this.bulletDom.style.bottom = this.positionY + "vh";
+    } else {
+      this.positionY -= 1;
+      this.bulletDom.style.bottom = this.positionY + "vh";
+    }
+  }
+}
+
+class Bullets3 {
+  constructor() {
+    this.width = 1;
+    this.height = 2;
+    this.positionX = 73 - this.width / 2;
+    this.positionY = 78;
+    this.bulletDom = null;
+
+    this.createBullets();
+  }
+
+  createBullets() {
+    this.bulletDom = document.createElement("div");
+    this.bulletDom.className = "bullet";
+    this.bulletDom.style.height = this.height + "vh";
+    this.bulletDom.style.width = this.width + "vw";
+    this.bulletDom.style.left = this.positionX + "vw";
+    this.bulletDom.style.bottom = this.positionY + "vh";
+    this.bulletParent = document.getElementById("enemy2");
+    this.bulletParent.appendChild(this.bulletDom);
+  }
+
+  moveDown(x) {
+    if (this.positionX > x) {
+      this.positionY -= 1;
+      this.positionX -= 0.5;
+      this.bulletDom.style.left = this.positionX + "vw";
+      this.bulletDom.style.bottom = this.positionY + "vh";
+    } else if (this.positionX < x) {
+      this.positionY -= 1;
+      this.positionX += 0.5;
+      this.bulletDom.style.left = this.positionX + "vw";
+      this.bulletDom.style.bottom = this.positionY + "vh";
+    } else {
+      this.positionY -= 1;
+      this.bulletDom.style.bottom = this.positionY + "vh";
+    }
+  }
+}
+
+class Meteor {
+  constructor() {
+    this.width = 4;
+    this.height = 6;
+    this.positionX = Math.floor(Math.random() * (100 - this.width) + 1);
+    this.positionY = 100 - this.height;
+    this.meteorDom = null;
+
+    this.createMeteor();
+  }
+
+  createMeteor() {
+    this.meteorDom = document.createElement("div");
+    this.meteorDom.className = "meteor";
+    this.meteorDom.style.height = this.height + "vh";
+    this.meteorDom.style.width = this.width + "vw";
+    this.meteorDom.style.left = this.positionX + "vw";
+    this.meteorDom.style.bottom = this.positionY + "vh";
+    this.meteorParent = document.getElementById("board");
+    this.meteorParent.appendChild(this.meteorDom);
+  }
+
+  moveDown() {
+    this.positionY -= 2;
+    this.meteorDom.style.bottom = this.positionY + "vh";
   }
 }
 
 class Ground {
   constructor() {
     this.width = 100;
-    this.height = 15;
+    this.height = 10;
     this.positionX = 0;
     this.positionY = 0;
     this.boxesArr = [];
 
-    this.createGroundContainer();
-    this.createGroundContainerBoxes();
+    this.createGround();
   }
 
-  createGroundContainer() {
+  createGround() {
     this.firstLayer = document.createElement("section");
-    this.firstLayer.id = "groundContainer";
+    this.firstLayer.id = "ground";
     this.groundParent = document.getElementById("board");
     this.groundParent.appendChild(this.firstLayer);
   }
-
-  createGroundContainerBoxes() {
-    for (let i = 0; i < 3; i++) {
-      this.groundBox = document.createElement("div");
-      this.groundBox.className = "groundBox";
-      this.groundBoxParent = document.getElementById("groundContainer");
-      this.groundBoxParent.appendChild(this.groundBox);
-    }
-  }
 }
 
-class ExitDoor {
-  constructor() {
-    this.height = 10;
-    this.width = 4;
-    this.positionX = 99 - this.width;
-    this.positionY = 15;
 
-    this.addExitDoor();
-  }
-
-  addExitDoor() {
-    this.exitDoor = document.createElement("div");
-    this.exitDoor.id = "exitDoor";
-    this.exitDoor.style.height = this.height + "vh";
-    this.exitDoor.style.width = this.width + "vw";
-    this.exitDoor.style.left = this.positionX + "vw";
-    this.exitDoor.style.bottom = this.positionY + "vh";
-    this.exitDoorParent = document.getElementById("board");
-    this.exitDoorParent.appendChild(this.exitDoor);
-  }
-}
 
 const newGame = new Game();
